f8c97ee3df29e566ef2bb7693127f3c6df01c6c1
MarcoChavezB <marco1102004@gmail.com>
2024-04-19 23:26:56 -0600

    añadiendo conexion mqtt

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from comunicacion import comunicacion
-#from controlador import Controlador
 import RPi.GPIO as GPIO
 from time import sleep
+import sys
+sys.path.append("/home/server/.local/lib/python3.9/site-packages")
+import paho.mqtt.client as mqtt
+import ssl
+import signal
+
+topic = "triggers/control"
+
+alarm_active = False
 
 GPIO.setmode(GPIO.BOARD)
 buzzer_pin = 40 
@@ -18,9 +26,10 @@ com = comunicacion()
 print("Iniciando")
 try:
     while True:
+        
         if(com.get_alarm_value() == 1):
             #controlador.alarma()
-            for _ in range(10):
+            for _ in range(2):
                 GPIO.output(buzzer_pin, GPIO.HIGH)
                 GPIO.output(led_left, GPIO.HIGH)
                 GPIO.output(led_right, GPIO.HIGH)
@@ -31,11 +40,13 @@ try:
                 sleep(1)
                 
             print(com.get_alarm_value(), "Alarma") 
+            
         elif(com.get_luz_value() > 900):
             #controlador.turn_on_lights()
             GPIO.output(led_left, GPIO.HIGH)
             GPIO.output(led_right, GPIO.HIGH)
             print(com.get_luz_value(),"Luz")
+            
         elif(com.get_luz_value() < 900):
             #controlador.turn_off_lights()
             GPIO.output(led_left, GPIO.LOW)
@@ -50,3 +61,39 @@ try:
             print(com.get_distance_value(),"Largo")
 except KeyboardInterrupt:
     pass
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado al broker MQTT con resultado: " + str(rc))        
+    client.subscribe(topic)
+    
+def on_message(client, userdata, msg):
+    global alarm_active
+    if(msg.payload.decode() == "v"):
+        alarm_active = True
+    elif(msg.payload.decode() == "b"):
+        alarm_active = False
+        
+def cleanup_gpio(signal, frame):
+    print("\nLimpiando pines GPIO...")
+    GPIO.cleanup()
+    sys.exit(0)
+    
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.tls_set(
+    ca_certs='/home/server/MQTT_Receptor/AmazonRootCA1.pem',
+    certfile='/home/server/MQTT_Receptor/cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-certificate.pem.crt',
+    keyfile='/home/server/MQTT_Receptor/cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-private.pem.key',
+    tls_version=ssl.PROTOCOL_SSLv23)
+client.tls_insecure_set(True)
+
+
+"""sleep(15)"""
+client.connect("a169mg5ru5h2z1-ats.iot.us-east-2.amazonaws.com", 8883, 60)
+
+signal.signal(signal.SIGINT, cleanup_gpio)
+
+client.loop_forever()
