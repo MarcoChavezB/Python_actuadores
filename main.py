f18e4e2b3bd7040792eb63a19583d760fa57b665
MarcoChavezB <marco1102004@gmail.com>
2024-04-19 23:14:04 -0600

    cambiando logica

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from comunicacion import comunicacion
 import RPi.GPIO as GPIO
 from time import sleep
 
+GPIO.setmode(GPIO.BOARD)
+buzzer_pin = 40 
+led_left = 33
+led_right = 37
+GPIO.setup(buzzer_pin, GPIO.OUT)
+GPIO.setup(led_left, GPIO.OUT)
+GPIO.setup(led_right, GPIO.OUT)
+
+
 com = comunicacion()
 #controlador = Controlador()
 
@@ -11,10 +20,23 @@ try:
     while True:
         if(com.get_alarm_value() == 1):
             #controlador.alarma()
+            for _ in range(10):
+                GPIO.output(buzzer_pin, GPIO.HIGH)
+                GPIO.output(led_left, GPIO.HIGH)
+                GPIO.output(led_right, GPIO.HIGH)
+                sleep(1)
+                GPIO.output(buzzer_pin, GPIO.LOW)
+                GPIO.output(led_left, GPIO.LOW)
+                GPIO.output(led_right, GPIO.LOW)
+                sleep(1)
+                
             print(com.get_alarm_value(), "Alarma") 
         elif(com.get_luz_value() > 900):
             #controlador.turn_on_lights()
+            GPIO.output(led_left, GPIO.HIGH)
+            GPIO.output(led_right, GPIO.HIGH)
             print(com.get_luz_value(),"Luz")
+            
         elif(com.get_distance_value() < 10):
             #controlador.short_distance()
             print(com.get_distance_value(), "Corto")
