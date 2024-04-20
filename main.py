94f8ed908d20c03b96efff8a4fcf54d1dd7f402f
MarcoChavezB <marco1102004@gmail.com>
2024-04-19 23:33:19 -0600

    limpiando codigo

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import signal
 topic = "triggers/control"
 
 alarm_active = False
+com = comunicacion()
 
 GPIO.setmode(GPIO.BOARD)
 buzzer_pin = 40 
@@ -19,36 +20,24 @@ GPIO.setup(buzzer_pin, GPIO.OUT)
 GPIO.setup(led_left, GPIO.OUT)
 GPIO.setup(led_right, GPIO.OUT)
 
-
-com = comunicacion()
-#controlador = Controlador()
-
-print("Iniciando")
-
+# Definir funciones de callback MQTT
 def on_connect(client, userdata, flags, rc):
-    print("Conectado al broker MQTT con resultado: " + str(rc))        
+    print("Conectado al broker MQTT con resultado: " + str(rc))
     client.subscribe(topic)
-    
+
 def on_message(client, userdata, msg):
     global alarm_active
-    if(msg.payload.decode() == "v"):
+    if msg.payload.decode() == "v":
         alarm_active = True
-    elif(msg.payload.decode() == "b"):
+    elif msg.payload.decode() == "b":
         alarm_active = False
 
-
-def cleanup_gpio(signal, frame):
-    print("\nLimpiando pines GPIO...")
-    GPIO.cleanup()
-    GPIO.output(buzzer_pin, GPIO.LOW)
-    GPIO.output(led_left, GPIO.LOW)
-    GPIO.output(led_right, GPIO.LOW)
-    sys.exit(0)
-    
+# Crear instancia del cliente MQTT
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+# Configurar TLS
 client.tls_set(
     ca_certs='/home/server/MQTT_Receptor/AmazonRootCA1.pem',
     certfile='/home/server/MQTT_Receptor/cec69141d6f3a0869a78f2331a3b6acebf6bc9ddb27a738dc3945c2ea4a99618-certificate.pem.crt',
@@ -56,14 +45,28 @@ client.tls_set(
     tls_version=ssl.PROTOCOL_SSLv23)
 client.tls_insecure_set(True)
 
+# Conectar al broker MQTT
+client.connect("a169mg5ru5h2z1-ats.iot.us-east-2.amazonaws.com", 8883, 60)
 
+# Función para limpiar GPIO
+def cleanup_gpio(signal, frame):
+    print("\nLimpiando pines GPIO...")
+    GPIO.cleanup()
+    sys.exit(0)
 
+# Configurar señal SIGINT para limpieza de GPIO
+signal.signal(signal.SIGINT, cleanup_gpio)
 
+# Esperar a que se establezca la conexión MQTT
+client.loop_start()
+while not client.is_connected():
+    sleep(0.1)
 
+# Después de conectarse, ejecutar el bucle principal
 try:
+    print("Conexión MQTT establecida. Iniciando bucle principal.")
     while True:
-        if(com.get_alarm_value() == 1):
-            #controlador.alarma()
+        if com.get_alarm_value() == 1 and alarm_active:
             for _ in range(2):
                 GPIO.output(buzzer_pin, GPIO.HIGH)
                 GPIO.output(led_left, GPIO.HIGH)
@@ -73,34 +76,22 @@ try:
                 GPIO.output(led_left, GPIO.LOW)
                 GPIO.output(led_right, GPIO.LOW)
                 sleep(1)
-                
-            #print(com.get_alarm_value(), "Alarma") 
-            
-        elif(com.get_luz_value() > 900):
-            #controlador.turn_on_lights()
+            print(com.get_alarm_value(), "Alarma") 
+
+        elif com.get_luz_value() > 900:
             GPIO.output(led_left, GPIO.HIGH)
             GPIO.output(led_right, GPIO.HIGH)
-            #print(com.get_luz_value(),"Luz")
-            
-        elif(com.get_luz_value() < 900):
-            #controlador.turn_off_lights()
+            print(com.get_luz_value(), "Luz")
+
+        elif com.get_luz_value() < 900:
             GPIO.output(led_left, GPIO.LOW)
             GPIO.output(led_right, GPIO.LOW)
-            #print(com.get_luz_value(),"Oscuro")
-    
-        elif(com.get_distance_value() < 10):
-            #controlador.short_distance()
+            print(com.get_luz_value(), "Oscuro")
+
+        elif com.get_distance_value() < 10:
             print(com.get_distance_value(), "Corto")
-        elif(com.get_distance_value() > 11):
-            #controlador.long_distance()
-            print(com.get_distance_value(),"Largo")
+        elif com.get_distance_value() > 11:
+            print(com.get_distance_value(), "Largo")
+
 except KeyboardInterrupt:
     pass
-
-
-"""sleep(15)"""
-client.connect("a169mg5ru5h2z1-ats.iot.us-east-2.amazonaws.com", 8883, 60)
-
-signal.signal(signal.SIGINT, cleanup_gpio)
-
-client.loop_forever()
