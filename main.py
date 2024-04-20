b64c3cb639971385c6d8328f7433b273a9b58ebd
MarcoChavezB <marco1102004@gmail.com>
2024-04-19 22:32:12 -0600

    añadiendo actuadores

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ try:
     while True:
         if(com.get_alarm_value() == 1):
             controlador.alarma()
+            print("Alarma")
         elif(com.get_luz_value() > 900):
             controlador.turn_on_lights()
+            print("Luz")
         elif(com.get_distance_value() < 10):
             controlador.short_distance()
+            print("Corto")
         elif(com.get_distance_value() > 11):
             controlador.long_distance()
+            print("Largo")
             
         
 except KeyboardInterrupt:
