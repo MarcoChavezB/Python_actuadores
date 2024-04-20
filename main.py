40a2a96b95a598420b3732acf462adbf9e55fc0d
MarcoChavezB <marco1102004@gmail.com>
2024-04-19 23:07:26 -0600

    quitando controlador

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from comunicacion import comunicacion
-from controlador import Controlador
+#from controlador import Controlador
 import RPi.GPIO as GPIO
 from time import sleep
 
 com = comunicacion()
-controlador = Controlador()
+#controlador = Controlador()
 
 print("Iniciando")
 try:
     while True:
         if(com.get_alarm_value() == 1):
-            controlador.alarma()
+            #controlador.alarma()
             print(com.get_alarm_value(), "Alarma") 
         elif(com.get_luz_value() > 900):
-            controlador.turn_on_lights()
+            #controlador.turn_on_lights()
             print(com.get_luz_value(),"Luz")
         elif(com.get_distance_value() < 10):
-            controlador.short_distance()
+            #controlador.short_distance()
             print(com.get_distance_value(), "Corto")
         elif(com.get_distance_value() > 11):
-            controlador.long_distance()
+            #controlador.long_distance()
             print(com.get_distance_value(),"Largo")
 except KeyboardInterrupt:
     pass
