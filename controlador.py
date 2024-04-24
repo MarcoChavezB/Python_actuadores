adf561279080e0e5c58d89a174b5d3ca6b661ec7
MarcoChavezB <marco1102004@gmail.com>
2024-04-23 23:20:29 -0600

    añadiendo codigo test en controlador

--- controlador.py
+++ controlador.py
@@ -1,42 +1,4 @@
-import RPi.GPIO as GPIO
-import time
+from comunicacion import comunicacion
 
-class Controlador:
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        self.buzzer_pin = 40 
-        self.led_left = 33
-        self.led_right = 37
-        GPIO.setup(self.buzzer_pin, GPIO.OUT)
-        GPIO.setup(self.led_left, GPIO.OUT)
-        GPIO.setup(self.led_right, GPIO.OUT)
-
-    def alarma(self, repetitions=10):
-        for _ in range(repetitions):
-            GPIO.output(self.buzzer_pin, GPIO.HIGH)
-            GPIO.output(self.led_left, GPIO.HIGH)
-            GPIO.output(self.led_right, GPIO.HIGH)
-            time.sleep(1)
-            GPIO.output(self.buzzer_pin, GPIO.LOW)
-            GPIO.output(self.led_left, GPIO.LOW)
-            GPIO.output(self.led_right, GPIO.LOW)
-            time.sleep(1)
-            
-    def stopAlarm(self):
-        GPIO.output(self.buzzer_pin, GPIO.LOW)
-        GPIO.output(self.led_left, GPIO.LOW)
-        GPIO.output(self.led_right, GPIO.LOW)
-        
-    def turn_on_lights(self):
-        GPIO.output(self.led_left, GPIO.HIGH)
-        GPIO.output(self.led_right, GPIO.HIGH)
-        
-    def turn_off_lights(self):
-        GPIO.output(self.led_left, GPIO.LOW)
-        GPIO.output(self.led_right, GPIO.LOW)
-        
-    def short_distance(self):
-        GPIO.output(self.buzzer_pin, GPIO.HIGH)
-        
-    def long_distance(self):
-        GPIO.output(self.buzzer_pin, GPIO.LOW)
+comunicacion = comunicacion()
+comunicacion.print_serial_data()
